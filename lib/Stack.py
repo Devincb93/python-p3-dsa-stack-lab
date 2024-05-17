d55fdedd480971efe8571f6f2627e235f64c80f5
Devin <devincb93@gmail.com>
2024-05-16 21:16:56 -0700

    complete

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,50 @@
 class Stack:
+    def __init__(self, items = None, limit = 100):
+        if items is None:
+            items = []
+        self.items = items
+        self.limit = limit
 
-    def __init__(self, items = [], limit = 100):
-        pass
-
-    def isEmpty(self):
-        pass
-
+    def empty(self):
+        if len(self.items) == 0:
+            return True
+        else:
+            return False
+        
+        
     def push(self, item):
-        pass
+        if not self.full():  # Check if stack is full before pushing
+            self.items.append(item)
+        else:
+            raise OverflowError("push to full stack")
+        
 
     def pop(self):
-        pass
+        if not self.empty():
+            return self.items.pop()
+        else:
+            raise IndexError("pop from empty stack")
+        
 
     def peek(self):
-        pass
+        if not self.empty():
+            return self.items[-1]
+        else:
+            raise IndexError("peek from empty stack")
+        
     
     def size(self):
-        pass
+        return len(self.items)
+        
 
     def full(self):
-        pass
+        return len(self.items) == self.limit
+            
+        
 
     def search(self, target):
-        pass
+        for i, item in enumerate(self.items):
+            if item == target:
+                return i
+        return self.items[-1]
+        
